31fef4e9a317003d99133058eb4ccbcdcaa6ccfc
Michael Sacks <michaelsacks@gmail.com>
2015-04-30 11:09:26 -0600

    Initial Commit

---
+++ TableViewsAreFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewsAreFun
+//
+//  Created by Michael Sacks on 4/30/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewsAreFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewsAreFun
+//
+//  Created by Michael Sacks on 4/30/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
